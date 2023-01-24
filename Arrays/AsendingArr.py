62de9fff46f75288d412fcd7075445becbd640a3
harshu <harshalbhogal66@gmail.com>
2023-01-24 17:12:43 +0530

    merge to Arrays

--- Arrays/AsendingArr.py
+++ Arrays/AsendingArr.py
@@ -1,9 +1,10 @@
 # Input will be the a = [1,2,0,3,1]
 #Here we need to use three pointer approch 
+#Time complexity is O(n)
 a = [0,1,2,0,1,2]
 
-l = 0 #-- 0
-m = 0 #-- 0
+l = 0 
+m = 0
 h = len(a)-1
 
 while m<=h:
@@ -16,4 +17,16 @@ while m<=h:
     else:
         a[m],a[h] = a[h],a[m]
         h = h-1
-print(a)
+# print(a)
+
+
+
+a1 = [0,2,1,2,0]
+
+# Here will used swapping
+# Time complexity is O(n2)
+for i in range(len(a1)):
+    for j in range(i+1,len(a1)):
+        if a1[i] > a1[j]:
+            a1[i],a1[j] = a1[j],a1[i]
+print(a1)
