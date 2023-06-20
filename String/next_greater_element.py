7edf9d0623fb487a5371b38cdaccc10a57828fa6
harshu <harshalbhogal66@gmail.com>
2023-06-20 15:58:42 +0530

    modification

--- String/next_greater_element.py
+++ String/next_greater_element.py
@@ -1,23 +1,31 @@
-def nextgreaterelement(ele):
-    n = len(ele)
-    for i in range(n-2,-1,-1):   # 132465
-        if ele[i] < ele[i+1]:
-            break
-    for j in range(n-1,i,-1):
-        print(ele[j])
-        break
-    # swaping
-    ele[i],ele[j] = ele[j],ele[i]
+def nextPermutation( nums):
+        
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        if n <= 2:
+            return nums.reverse()
+        pointer = n - 2
 
-    #reversed the list
-    ele[i+1:] = reversed(ele[i+1:])
+        while pointer >= 0 and nums[pointer] >= nums[pointer+1]:
+            pointer -= 1
+        
+        if pointer == -1:
+            return nums.reverse()
+        for j in range(n-1,pointer,-1):
+            if nums[pointer] < nums[j]:
+                nums[pointer],nums[j] = nums[j],nums[pointer]
+                break
+        nums[pointer+1:] = reversed(nums[pointer+1:])
 
     #Converting list into the array
-    a = 0
-    for k in range(n):
-        a = a*10 + ele[k]
-    print(a)
+        a = 0
+        for k in range(n):
+            a = a*10 + nums[k]
+        print(a)
 
 ar = "132465"
 ar= list(map(int,ar))
-nextgreaterelement(ar)
+nextPermutation(ar)
